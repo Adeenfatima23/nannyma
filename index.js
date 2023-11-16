ac03844ebd14d961d8b4a318583cfd4c3fa30f2c
Shabbirjodhpur <shabbirjodhpurwala@gmail.com>
2023-11-16 15:36:58 +0530

    made form functional

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 console.log('start')
-function contactUs(){
+document.getElementById('contact-us').addEventListener('click',()=>{
 let name = document.getElementById('name').value
 let surname = document.getElementById('surname').value
 let email = document.getElementById('email').value
@@ -20,5 +20,5 @@ let website = document.getElementById('website').value
 		alert('Website is required.')
 		return;	
 	}
-	window.location.replace(`https://wa.me/919618185563?text=Name%3A%20${name}%0ASurname%3A%20${surname}%0AEmail%3A%20${email}%0AWebsite%3A%20${website}`)
-}
+	window.location.href = `https://api.whatsapp.com/send?phone=919618185563&text=Name%3A%20${encodeURI(name)}%0ASurname%3A%20${encodeURI(surname)}%0AEmail%3A%20${encodeURI(email)}%0AWebsite%3A%20${encodeURI(website)}`;
+})
