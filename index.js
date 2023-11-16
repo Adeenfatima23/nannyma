0e41db39df7be1bd09cc930f308c72a0fed87d66
Shabbirjodhpur <shabbirjodhpurwala@gmail.com>
2023-11-16 15:08:23 +0530

    made form functional

--- index.js
+++ index.js
@@ -5,16 +5,20 @@ let surname = document.getElementById('surname').value
 let email = document.getElementById('email').value
 let website = document.getElementById('website').value
 	if(name == ''){
-		alert('Name is required.')	
+		alert('Name is required.')
+		return;	
 	}
 	if(surname==''){
 		alert('Surname is required.')	
+		return;
 	}
 	if(email==''){
-		alert('Email is required.')	
+		alert('Email is required.')
+		return;	
 	}
 	if(website==''){
-		alert('Website is required.')	
+		alert('Website is required.')
+		return;	
 	}
 	window.location = `https://wa.me/+919618185563?text=Name%3A%20${name}%0ASurname%3A%20${surname}%0AEmail%3A%20${email}%0AWebsite%3A%20${website}`
 }
